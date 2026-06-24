b25289f47f8452a74616678b8ab0951938ea08e8
Irshad <irshakhan012003@gmail.com>
2026-06-24 13:53:12 +0530

    Fixing smtp error

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -81,13 +81,14 @@ exports.forgetPassword = async (req, res) => {
 
  const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 10000
+  family: 4, // FORCE IPv4 (IMPORTANT FIX)
+  connectionTimeout: 20000
 });
 
    await transporter.verify();
