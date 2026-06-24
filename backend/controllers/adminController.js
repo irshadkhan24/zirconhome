d8c9dd7705c7c97f974165639a59823f7012a36f
Irshad <irshakhan012003@gmail.com>
2026-06-24 12:22:02 +0530

    Fix Gmail SMTP

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -79,14 +79,15 @@ exports.forgetPassword = async (req, res) => {
     const resetURL =
       `https://www.zirconhome.com/resetpassword.html?token=${resetToken}`;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     await transporter.verify();
 console.log("SMTP Connected");
